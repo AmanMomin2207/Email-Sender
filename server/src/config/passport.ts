b3d0202691c807246129764754c9dee47f3b682d
AmanMomin2207 <amanchandsomomin7860@gmail.com>
2026-01-22 15:10:07 +0530

    update passport.ts

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -18,11 +18,11 @@ passport.deserializeUser(async (id: string, done) => {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID || 'dummy',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'dummy',
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL!,
 },
-    async (accessToken, refreshToken, profile, done) => {
+    async (_accessToken, _refreshToken, profile, done) => {
         try {
             const email = profile.emails?.[0].value;
             if (!email) return done(new Error('No email found'), undefined);
